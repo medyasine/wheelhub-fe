e395ab2febe3b536a0f262db3e1b91373c9e3671
Aminben2 <aminbenomar454@gmail.com>
2024-08-21 11:19:04 +0100

    prime react ready

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { PrimeReactProvider } from "primereact/api";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+createRoot(document.getElementById("root")).render(
+  <PrimeReactProvider
+    value={{
+      unstyled: false,
+    }}
+  >
     <App />
-  </StrictMode>,
-)
+  </PrimeReactProvider>
+);
