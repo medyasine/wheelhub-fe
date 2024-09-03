fff0c242926631e35854056aae1aa29374d0d824
yasssin <yasssin@gmail.com>
2024-09-03 12:17:54 +0100

    fix toggle

--- src/pages/layouts/RootLayout.jsx
+++ src/pages/layouts/RootLayout.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 
 function RootLayout() {
+  const [isLoginModalVisible, setLoginModalVisible] = useState(false);
+
+  const toggleLoginModal = () => {
+    setLoginModalVisible(!isLoginModalVisible);
+  };
+
   return (
     <div className="root-layout h-screen scroll-smooth">
       <Outlet setShowLogin={toggleLoginModal} />
